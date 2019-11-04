c89ac79306422e0f3e79000a2f2027794bf0a72d
Travis <travisstew214@gmail.com>
2019-11-03 20:42:31 -0500

    files and folders created

--- FriendFinder/app/server.js
+++ FriendFinder/app/server.js
@@ -7,19 +7,12 @@ var PORT = process.env.PORT || 8080;
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
-// var urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.use(bodyParser.json());
 
 //api routes
 var api = require('../app/routing/apiRoutes');
 api(app);
 
-
-// app.post('/api/friends',urlencodedParser, function (req,res) {
-//   res.end(JSON.stringify(req.body));
-//   console.log(req.body);
-
-// });
 //html routes
 var routes = require('../app/routing/htmlRoutes');
 routes(app);
